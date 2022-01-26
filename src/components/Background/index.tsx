3f5c8ead5c85a5a537079f80a47e132548269ba8
Tim <timour.almazov@gmail.com>
2022-01-26 22:57:10 +0100

    init nextjs

--- src/components/Background/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-interface Props {
-  children: React.ReactNode;
-}
-
-export default ({ children }: Props) => (
-  <div className="p-10 h-screen bg-black text-white">{children}</div>
-);
--- src/components/index.tsx
+++
@@ -1,3 +0,0 @@
-export { default as Background } from './Background';
-export { default as Header } from './Header';
-export { default as Button } from './Button';
